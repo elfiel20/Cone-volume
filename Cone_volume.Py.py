3039b79211d8b7f310527d7d00fc65d60e57e9c4
elfiel20 <98498077+elfiel20@users.noreply.github.com>
2022-02-01 18:16:51 -0500

    Add files via upload

---
+++ Cone_volume.Py.py
@@ -0,0 +1,18 @@
+#Exercise 5
+#This is a program to compute and provide the area of a cine
+
+#line 5 asks for the radius from the user and turns it into a float
+r=float(input("Enter the radius of the cone:"))
+
+#line 8 asks for the height of the cone from the user and turns it into a float
+h=float(input("Enter the height of the cone:"))
+
+#the below is establishing a value for pi
+pi=3.14159
+
+print(("v=", pi*r**2*h/3),2)
+
+
+
+
+
